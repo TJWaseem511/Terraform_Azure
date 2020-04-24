9d8e81972444ada04a1ff2ee520f714ef72fd37d
unknown <TCSHYDISUSER@01hw721145.ERICITINHYD.COM>
2020-04-24 11:28:02 +0530

    Add virtual network

--- main.tf
+++ main.tf
@@ -5,6 +5,57 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rgs"{
+  # Common resource group
     location = "east us 2"
     name = var.rg_name
+}
+
+resource "azurerm_network_security_group" "sec_grp" {
+  # A security group to secure Virtual network
+  name                = "acceptanceTestSecurityGroup1"
+  location            = azurerm_resource_group.rgs.location
+  resource_group_name = azurerm_resource_group.rgs.name
+}
+
+resource "azurerm_network_ddos_protection_plan" "ddos_p_plan" {
+  # A DDOS plan to repel DDOS attack
+  name                = "ddospplan1"
+  location            = azurerm_resource_group.rgs.location
+  resource_group_name = azurerm_resource_group.rgs.name
+}
+
+resource "azurerm_virtual_network" "virt_net" {
+  # The actual virtual network
+  name                = "dummy_vir_net"
+  location            = azurerm_resource_group.rgs.location
+  resource_group_name = azurerm_resource_group.rgs.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  ddos_protection_plan {
+    # Assigning the created ddos plan
+    id     = azurerm_network_ddos_protection_plan.ddos_p_plan.id
+    enable = true
+  }
+
+  # Subnets
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+  }
+
+  subnet {
+    name           = "subnet3"
+    address_prefix = "10.0.3.0/24"
+    security_group = azurerm_network_security_group.sec_grp.id
+  }
+
+  tags = {
+    environment = "learning"
+  }
 }
--- output.tf
+++ output.tf
@@ -1,3 +1,6 @@
-output "rg_id"{
+output "resource_group_id"{
   value = azurerm_resource_group.rgs.id
+}
+output "vNet_name"{
+  value = azurerm_virtual_network.virt_net.name
 }
